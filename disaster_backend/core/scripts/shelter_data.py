3dbcd986d36f6e683f92f1b03d2e1325343f8f58
Ptl-sneh <snehpatel641@gmail.com>
2025-08-06 16:54:06 +0530

    Created Data for the prediction model

---
+++ disaster_backend/core/scripts/shelter_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import random
+import os
+
+disaster_types = ['Fire','Flood','Earthquake','Storm/Hurricane','Drought','Volcanic Eruption','Tornado','Cyclone','Tsunami','Landslide']
+
+def generate_data(row_num):
+    data = []
+    for _ in range(row_num):
+        capacity = random.randint(50,1000)
+        current_occupancy = random.randint(30,capacity)
+        disaster_type = random.choice(disaster_types)
+        
+        food_needed = current_occupancy * random.randint(2,6)
+        water_required = current_occupancy * random.randint(8,15)
+        medical_kits = max(1,current_occupancy//random.randint(10,28))
+        volunteers_required = max(2, current_occupancy // random.randint(15, 38))
+        
+        data.append({
+            'capacity' : capacity,
+            'current_occupancy' : current_occupancy,
+            'disaster_type' : disaster_type,
+            'food_needed' : food_needed,
+            'water_required' : water_required,
+            'medical_kits' : medical_kits,
+            'volunteers_required' : volunteers_required
+        })
+    return pd.DataFrame(data)
+
+shelter_df = generate_data(10000)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+csv_path = os.path.join(script_dir,'Shelter_Resources_Prediction.csv')
+
+shelter_df.to_csv(csv_path,index = False)
+
+print('CSV created and saved')
